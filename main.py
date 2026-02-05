6adef53c87f85148b0d593c7de5a3cc8ad331d50
mr-pointing <rpointing@blmhs.org>
2026-02-05 09:09:59 -0500

    Added numbers and a person class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 
-nums = [1, 2, 3]
+nums = [1, 2, 3, 4, 5, 6]
 
 
 print(f"Your lucky number is {random.choice(nums)}")
---
+++ person.py
@@ -0,0 +1,5 @@
+class Person:
+
+    def __init__(self, age):
+        self.age = age
+
